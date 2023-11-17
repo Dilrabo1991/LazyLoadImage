a9c7b18bca89147b08eacdc51aa784ebc6c4006b
Dilrabo1991 <dilraboxujamuratova@gmail.com>
2023-11-17 10:34:13 +0500

    create ContentLoader component

--- src/App.js
+++ src/App.js
@@ -2,27 +2,14 @@
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import ReactPlayer from 'react-player';
-import Video from './components/video';
+import HeadBodyGrid from './components/ContentLoaderComponent';
+
 
 const App = () => {
   const url = 'https://plus.unsplash.com/premium_photo-1674676471380-1258cb31b3ac?q=80&w=2009&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
   const imges = " "
-  const error = "https://www.istockphoto.com/photo/404-error-isolated-on-white-background-page-not-found-gm1481759725-508976803?utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2F404&utm_medium=affiliate&utm_source=unsplash&utm_term=404%3A%3A%3A"
+  const defaultimg = "https://us.123rf.com/450wm/wasiliyg/wasiliyg1702/wasiliyg170200583/71886379-snapshot-vecteur-ic%C3%B4ne-image-vecteur-ic%C3%B4ne.jpg?ver=6"
 
-  const urlv = "https://www.youtube.com/watch?v=NpEaa2P7qZI"
-  const video = "https://www.youtube.com/watch?v=Xc3rOhoAGtc "
-  const plaseholder = {
-    src: video,
-    alt: 'video',
-    width: 1000,
-    height: 500
-  }
-  const videos = {
-    src: urlv,
-    alt: 'video',
-    width: 1500,
-    height: 700
-  }
 
   const image = {
     src: url,
@@ -36,23 +23,13 @@ const App = () => {
     width: 600,
     height: 700
   }
-  const errorimg = {
-    src: error,
+  const defaultPlaseholder = {
+    src: defaultimg,
     alt: 'image',
     width: 600,
-    height: 700
+    height: 450
   }
-  if (imges === error) {
-    return (
-      <LazyLoadImage
-        alt={errorimg.alt}
-        height={errorimg.height}
-        effect="blur"
-        placeholderSrc={errorimg.src}
-        src={errorimg.src}
-        width={errorimg.width} />
-    )
-  }
+
   return (
     <div className="App">
       <LazyLoadImage
@@ -63,28 +40,19 @@ const App = () => {
         src={image.src}
         width={image.width} />
       <LazyLoadImage
-        alt={img.alt}
+        alt={defaultPlaseholder.alt}
         height={img.height}
         effect="blur"
-        placeholderSrc={image.src}
-        src={img.src}
+        placeholderSrc={defaultPlaseholder.src}
+        src={defaultPlaseholder.src}
         width={img.width} />
-      <ReactPlayer
-        className="reactplayer"
-        alt={videos.alt}
-        bgColor="black"
-        placeholderSrc={videos.src}
-        url={plaseholder.src}
-        playing={true}
-        width={plaseholder.width}
-        controller
-        loop
-        height={plaseholder.height}
-      />
-      <Video />
 
+      <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />
+      <HeadBodyGrid />
     </div>
 
+    // default img //! placeholderSrc={defaultPlaseholder.src} 
+    //? lezyload component  zo'r ekan.
 
 
   )
